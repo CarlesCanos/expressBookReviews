886990239362fe42900d660355973344aec33010
carles <carlescanos@gmail.com>
2025-05-31 07:47:44 -0400

    first 3 exercises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,26 +6,40 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status().json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if(isbn){
+        return res.status(200).json(books[isbn]);
+    }
+    return res.status(400).json({message: "Book not found"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    console.log(author)
+    if(author){
+        let targetBook = -1;
+        for (let index = 1; index < 11; index++) {
+            console.log(author + " " +  books[index].author);
+            if(author.toString() == books[index].author){
+                targetBook = index;
+            }
+        }
+        if(targetBook != -1){
+         return res.status(200).json(books[targetBook]);
+        }
+    }
+    return res.status(400).json({message: "Book not found"});
 });
 
 // Get all books based on title
